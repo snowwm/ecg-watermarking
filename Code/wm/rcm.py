758ea2b095754068da4c7eca94b520e69ea9b795
snowwm <snowwm.dev@gmail.com>
2023-06-19 15:40:55 +0400

    Rename DE to RCM

--- Code/wm/rcm.py
+++ Code/wm/rcm.py
@@ -5,43 +5,44 @@ import errors
 from .base import WMBase
 
 
-class DEEmbedder(WMBase):
-    codename = "de"
+class RCMEmbedder(WMBase):
+    codename = "rcm"
     packed_block_type = np.uint8
     test_matrix = {
         "wm_cont_len": [(83, 1000), (2000, 18000)],
         "redundancy": [1, 2],
-        "de_rand_shift": [False, True],
-        "de_shift": [1, 3],
+        "rcm_rand_shift": [False, True],
+        "rcm_shift": [1, 3],
+        "block_len": [1, 4],
     }
 
-    def __init__(self, de_shift=1, de_rand_shift=False, de_skip=True, **kwargs):
+    def __init__(self, rcm_shift=1, rcm_rand_shift=False, rcm_skip=True, **kwargs):
         super().__init__(**kwargs)
         if self.block_len > 8:
             raise errors.InvalidConfig(suffix="block_len > 8 not supported")
 
-        self.de_shift = de_shift
-        self.de_rand_shift = de_rand_shift
-        self.de_skip = de_skip
+        self.rcm_shift = rcm_shift
+        self.rcm_rand_shift = rcm_rand_shift
+        self.rcm_skip = rcm_skip
 
-        self.de_n = 2 ** (self.block_len - 1)
-        self.de_mod = 2 * self.de_n + 1
-        self.de_k1 = (self.de_n + 1) / self.de_mod
-        self.de_k2 = self.de_n / self.de_mod
+        self.rcm_n = 2 ** (self.block_len - 1)
+        self.rcm_mod = 2 * self.rcm_n + 1
+        self.rcm_k1 = (self.rcm_n + 1) / self.rcm_mod
+        self.rcm_k2 = self.rcm_n / self.rcm_mod
 
     @property
     def max_restore_error(self):
-        return self.de_n * 2
+        return self.rcm_n * 2
 
     def get_coords(self, carr):
-        c1 = np.arange(0, len(carr) - self.de_shift, self.de_shift + 1)
+        c1 = np.arange(0, len(carr) - self.rcm_shift, self.rcm_shift + 1)
         if not self.contiguous:
             self.rng().shuffle(c1)
 
-        if self.de_rand_shift:
-            c2 = c1 + self.rng().integers(1, 1 + self.de_shift, len(c1))
+        if self.rcm_rand_shift:
+            c2 = c1 + self.rng().integers(1, 1 + self.rcm_shift, len(c1))
         else:
-            c2 = c1 + self.de_shift
+            c2 = c1 + self.rcm_shift
 
         return np.column_stack((c1, c2))
 
@@ -51,7 +52,7 @@ class DEEmbedder(WMBase):
         # Prevent overflow by using a "big" type.
         x1 = self.container[c1].astype(np.int64)
         x2 = self.container[c2].astype(np.int64)
-        n = self.de_n
+        n = self.rcm_n
         y1 = (n + 1) * x1 - n * x2
         y2 = (n + 1) * x2 - n * x1
 
@@ -68,19 +69,19 @@ class DEEmbedder(WMBase):
 
         # Embed embeddable.
         w = wm[: y1e.size].astype(np.int16) + 1
-        w[w > n] -= self.de_mod
+        w[w > n] -= self.rcm_mod
         self.carrier[c1[embeddable]] = y1e + w
         self.carrier[c2[embeddable]] = y2e
 
         if x1n.size > 0:
-            if not self.de_skip:
-                raise errors.CantEmbed(suffix="range overflow and de_skip is off")
+            if not self.rcm_skip:
+                raise errors.CantEmbed(suffix="range overflow and rcm_skip is off")
 
             # Modify non-embeddable.
-            r = (x1n - x2n) % self.de_mod
+            r = (x1n - x2n) % self.rcm_mod
             v1 = x1n - r
             v1_fits = (min2 <= v1) & (v1 <= max2)
-            v2 = v1 + self.de_mod
+            v2 = v1 + self.rcm_mod
             v2_fits = (min2 <= v2) & (v2 <= max2)
             # One of (v1, v2) must fit into the range.
             if not (v1_fits | v2_fits).all():
@@ -94,9 +95,9 @@ class DEEmbedder(WMBase):
         c2 = coords[:, 1]
         y1 = self.carrier[c1].astype(np.int64)
         y2 = self.carrier[c2].astype(np.int64)
-        w = (y1 - y2) % self.de_mod
+        w = (y1 - y2) % self.rcm_mod
 
-        if self.de_skip:
+        if self.rcm_skip:
             filled = w != 0
         else:
             filled = np.ones_like(w, dtype=bool)
@@ -109,11 +110,11 @@ class DEEmbedder(WMBase):
         wm[: w.size] = (w - 1).astype(self.packed_block_type)
 
         # Restore original.
-        w[w > self.de_n] -= self.de_mod
+        w[w > self.rcm_n] -= self.rcm_mod
         y1 -= w
-        x1 = np.round(self.de_k1 * y1 + self.de_k2 * y2)
-        x2 = np.round(self.de_k2 * y1 + self.de_k1 * y2)
+        x1 = np.round(self.rcm_k1 * y1 + self.rcm_k2 * y2)
+        x2 = np.round(self.rcm_k2 * y1 + self.rcm_k1 * y2)
+
         self.restored[c1[filled]] = x1
         self.restored[c2[filled]] = x2
-
         return w.size
